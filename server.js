af58d40c0abca5c7ffbd39931edca717617d1450
Reneh7 <renehm2001@gmail.com>
2023-06-22 13:37:25 +0300

    Lab12

--- server.js
+++ server.js
@@ -2,16 +2,17 @@
 
 const express = require("express");
 const app = express();
-app.listen(3001);
+const movie=require("./MovieData/data.json")
+const axios=require("axios")
+const e = express();
+require("dotenv").config();
+
+app.listen(3000);
 
 const cors=require("cors");
 app.use(cors());
 
 app.get("/", moviesInfoHandler);
-app.get("/favorite",handleFav)
-app.get("/error",serverError)
-app.get("*",notFoundError)
-
 function moviesInfoHandler(req, res) 
 {
 
@@ -22,23 +23,7 @@ function moviesInfoHandler(req, res)
      this.mOverview = overview;
   }
 
-    const movie = {
-        "title": "Spider-Man: No Way Home",
-        "genre_ids": [28, 12, 878],
-        "original_language": "en",
-        "original_title": "Spider-Man: No Way Home",
-        "poster_path": "/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
-        "video": false,
-        "vote_average": 8.4,
-        "overview": "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
-        "release_date": "2021-12-15",
-        "vote_count": 3160,
-        "id": 634649,
-        "adult": false,
-        "backdrop_path": "/1Rr5SrvHxMXHu5RjKpaMba8VTzi.jpg",
-        "popularity": 10039.54,
-        "media_type": "movie"
-    };
+ 
     const movieObj = new MoviesInfo(movie.title, movie.poster_path, movie.overview);
 
     res.json({
@@ -48,17 +33,44 @@ function moviesInfoHandler(req, res)
     });
 }
 
-function handleFav(req,res)
-{
-    res.send("Welcome to Favorite Page")
-}
+app.get("/favorite",(req,res) => send("Welcome to Favorite Page"))
 
-function serverError(req,res)
-{
-    res.status(500).send("Sorry, something went wrong");
-}
+app.get("/trending", async (req,res) => {
+    let trendingMovies = await axios.get(
+        `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.SecretAPI}&language=en-US`
+      );
+      const moviesData = trendingMovies.data.results.map(movie => ({
+        id: movie.id,
+        title: movie.title,
+        release_date: movie.release_date,
+        poster_path: movie.poster_path,
+        overview: movie.overview
+      }));
+      res.json(moviesData)
+    });
 
-function notFoundError(req,res)
-{
-    res.status(404).send("Page not found");
-}
+app.get("/search", async(req,res) => {
+    let MovieName = req.query.title;
+    let axiosResponse = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.SecretAPI}&language=en-US&query=${MovieName}`);
+    res.send(axiosResponse.data);
+  });
+
+app.get("/people", async(req,res) => {
+    let people= await axios.get(`https://api.themoviedb.org/3/person/changes?api_key=${process.env.SecretAPI}&language=en-US`)
+    const peopleData=people.data.results.map(person =>({
+        id:person.id,
+        name:person.adult
+    }))
+    res.json(peopleData);
+})  
+
+app.get("/tv_list", async (req,res)=>{
+    let list= await axios.get(`https://api.themoviedb.org/3/tv/changes?api_key=${process.env.SecretAPI}&language=en-US&page=1`)
+    console.log(list);
+    res.send(list.data);
+})
+
+app.use("/error",(req,res,next) => res.status(500).send("Sorry, something went wrong"))
+
+app.use("*",(req,res,next) => res.status(404).send("Page not found"))
+
